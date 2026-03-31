c81f5c1af88208803ea21045f317240b32604dae
Ahmed Montasser <ahmed.sharkway99@hotmail.com>
2026-03-31 22:11:14 +0200

    Initial Commit

---
+++ src/test/java/org/company/reservator/ReservatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.company.reservator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
